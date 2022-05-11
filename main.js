4a7ad9f489094dd7f348cc2a283d4a3642c31bcc
Emmanuel Paul <91189703+Epaltechs@users.noreply.github.com>
2022-05-11 12:09:37 +0100

    Edit package.json file

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function stringLength(practice) {
-        return practice.split('').reduce((length) => length + 1, 0);
+  return practice.split('').reduce((length) => length + 1, 0);
 }
 
 it('characters', () =>
