79523dd3524266cdba897ecb43fb01d5fdd1e756
Emmanuel Paul <91189703+Epaltechs@users.noreply.github.com>
2022-05-13 07:54:54 +0100

    Update main file

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
 exports.stringLength = (name, num) => {
   return `${name}(${num} 12)`;
 };
-
-// it('characters', () =>
-//   expect(stringLength({
-//   })).toBe(8))
-
-// function stringLength(practice) {
-//   return practice.split('').reduce((length) => length + 1, 0);
-// }
-
-// it('characters', () =>
-//   expect(stringLength({
-//   })).toBe(8))
