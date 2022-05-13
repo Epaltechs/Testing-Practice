ef1725bab06a13775e185cf1dc2b98ac080cdd7a
Emmanuel Paul <91189703+Epaltechs@users.noreply.github.com>
2022-05-13 07:53:21 +0100

    Seperate test file

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-function stringLength(practice) {
-  return practice.split('').reduce((length) => length + 1, 0);
-}
+exports.stringLength = (name, num) => {
+  return `${name}(${num} 12)`;
+};
 
-it('characters', () =>
-  expect(stringLength({
-  })).toBe(8))
+// it('characters', () =>
+//   expect(stringLength({
+//   })).toBe(8))
+
+// function stringLength(practice) {
+//   return practice.split('').reduce((length) => length + 1, 0);
+// }
+
+// it('characters', () =>
+//   expect(stringLength({
+//   })).toBe(8))
